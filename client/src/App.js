0f244cb2c70b0588880b42e2800995823b0f2d87
unknown <jacob.e.england@gmail.com>
2020-03-13 10:25:30 -0500

    Added class component with API call

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+      .then((response) => this.setState({ data: response.data }))
+  }
+  render() {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
 }
 
 export default App;
